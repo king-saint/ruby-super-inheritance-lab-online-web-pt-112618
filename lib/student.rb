d9c33568719fc8151ee9352e6189fb963d39f0b0
Wendi Chen <epanistamai.proles@gmail.com>
2019-01-13 22:31:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,10 @@
 class Student
 
   def hello
-    "Hey there! I'm so excited to learn about stuff."
+    return "Hey there! I'm so excited to learn about stuff."
   end
   
   def raise_hand
-    "Pick me!"
+    return "Pick me!"
   end
 end
